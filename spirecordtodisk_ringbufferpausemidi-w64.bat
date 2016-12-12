c8ec950cab7039a67041c2d882141dd7ad12e7c7
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 11:44:39 -0500

    first commit

---
+++ spirecordtodisk_ringbufferpausemidi-w64.bat
@@ -0,0 +1,2 @@
+rem spirecordtodisk_ringbufferpausemidi-w64.exe "recording.w64" 120 "E-MU ASIO" 2 3
+spirecordtodisk_ringbufferpausemidi-w64.exe "recording.w64" 120 "E-MU ASIO" 2 3 "In From MIDI Yoke:  1" 0 64
